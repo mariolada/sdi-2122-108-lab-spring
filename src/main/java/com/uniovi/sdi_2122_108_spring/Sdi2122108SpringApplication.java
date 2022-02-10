206c9dc6adc8abff4d0e724da4f0f5d9f722be68
Mario <UO275874@uniovi.es>
2022-02-10 11:53:52 +0100

    SDI-2122-108-2.1-Create Spring project.

---
+++ src/main/java/com/uniovi/sdi_2122_108_spring/Sdi2122108SpringApplication.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi_2122_108_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sdi2122108SpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Sdi2122108SpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/uniovi/sdi_2122_108_spring/Sdi2122108SpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uniovi.sdi_2122_108_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sdi2122108SpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
