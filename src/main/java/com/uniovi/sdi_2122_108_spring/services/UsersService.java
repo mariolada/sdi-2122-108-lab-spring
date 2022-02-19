8fcefb957c108978efeed17d0a9737f865ca9e02
Mario <UO275874@uniovi.es>
2022-02-19 14:14:55 +0100

    SDI-2122-108-3.4-Autenticación y control de acceso.

--- src/main/java/com/uniovi/sdi_2122_108_spring/services/UsersService.java
+++ src/main/java/com/uniovi/sdi_2122_108_spring/services/UsersService.java
@@ -9,10 +9,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import com.uniovi.sdi_2122_108_spring.entities.*;
 import com.uniovi.sdi_2122_108_spring.services.UsersService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+
 @Service
 public class UsersService {
     @Autowired
     private UsersRepository usersRepository;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
     public List<User> getUsers() {
         List<User> users = new ArrayList<User>();
@@ -23,8 +29,13 @@ public class UsersService {
     public User getUser(Long id) {
         return usersRepository.findById(id).get();
     }
+    public User getUserByDni(String dni) {
+        return usersRepository.findByDni(dni);
+    }
+
 
     public void addUser(User user) {
+        user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
         usersRepository.save(user);
     }
 
