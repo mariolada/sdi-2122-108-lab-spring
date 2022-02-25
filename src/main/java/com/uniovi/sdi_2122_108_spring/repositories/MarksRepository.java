f4f8170d424adfc54e58f6d0a5ddd54e885d1baa
Mario <UO275874@uniovi.es>
2022-02-25 16:14:59 +0100

    SDI-2122-108-4.5-SpringBoot-Búsqueda

--- src/main/java/com/uniovi/sdi_2122_108_spring/repositories/MarksRepository.java
+++ src/main/java/com/uniovi/sdi_2122_108_spring/repositories/MarksRepository.java
@@ -14,6 +14,12 @@ public interface MarksRepository extends CrudRepository<Mark, Long> {
     @Query("SELECT r FROM Mark r WHERE r.user = ?1 ORDER BY r.id ASC")
     List<Mark> findAllByUser(User user);
 
+    @Query("select r from Mark r where (lower(r.description) like lower(?1) or lower(r.user.name) like lower(?1))")
+    List<Mark> searchByDescriptionAndName(String searchtext);
+
+    @Query("select r from Mark r where (lower(r.description) like lower(?1) or lower(r.user.name) like lower(?1)) and r.user=?2")
+    List<Mark> searchByDescriptionNameAndUser(String searchtext, User user);
+
     @Modifying
     @Transactional
     @Query("UPDATE Mark SET resend = ?1 WHERE id = ?2")
